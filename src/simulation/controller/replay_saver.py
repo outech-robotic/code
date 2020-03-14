cf78452c8d2d7f29d7218ada553d89e0af4a40a5
Nicolas Bonnet <mail@nicolasbon.net>
2020-03-14 19:09:40 +0100

    Change replay URL (#64)

--- src/simulation/controller/replay_saver.py
+++ src/simulation/controller/replay_saver.py
@@ -12,7 +12,7 @@ from src.simulation.entity.simulation_configuration import SimulationConfigurati
 from src.simulation.entity.simulation_state import RobotID
 
 REPLAY_API_URL = 'https://replay-api.outech.fr/replay/'
-REPLAY_VIEWER_URL = 'https://nicolasbon.net/replay/'
+REPLAY_VIEWER_URL = 'https://outech-robotic.github.io/replay/index.html'
 # REPLAY_VIEWER_URL = 'http://127.0.0.1:8000/'
 
 
