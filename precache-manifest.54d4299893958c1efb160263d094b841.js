91b8d6bbfb9b0fcbdd09d89246d41166697b3632
Nicolas Bonnet <mail@nicolasbon.net>
2020-04-29 19:51:01 +0200

    Updates

---
+++ precache-manifest.54d4299893958c1efb160263d094b841.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d14140948848b7691c8afabe616b9bb",
+    "url": "/code/index.html"
+  },
+  {
+    "revision": "f02691a39a1445296202",
+    "url": "/code/static/css/main.4e3f5839.chunk.css"
+  },
+  {
+    "revision": "5884c9b742bd898fa1ab",
+    "url": "/code/static/js/2.5dc21458.chunk.js"
+  },
+  {
+    "revision": "3f9a62d9b984e80a68ae1fb6b253a0ab",
+    "url": "/code/static/js/2.5dc21458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f02691a39a1445296202",
+    "url": "/code/static/js/main.bddf57f0.chunk.js"
+  },
+  {
+    "revision": "baf840576f867bfa1fc9",
+    "url": "/code/static/js/runtime-main.9b956c97.js"
+  },
+  {
+    "revision": "a40c655a8721f0096d544982b6f73eab",
+    "url": "/code/static/media/plateau.a40c655a.svg"
+  },
+  {
+    "revision": "1ba7faeb57f846c92495996613e5f68a",
+    "url": "/code/static/media/robot.1ba7faeb.svg"
+  }
+]);
