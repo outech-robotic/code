bf8fd00a607b6aad96f5a8e00ffc306f9521252c
TicTacMenthe <ugurcan.ekren@gmail.com>
2020-04-30 20:55:49 +0200

    config for motor board g4 for can addresses

--- lowlevel/src/motor-board-g4/config.h
+++ lowlevel/src/motor-board-g4/config.h
@@ -39,8 +39,8 @@ void Error_Handler();
 
 // IDs used by board on CAN interface
 #define CONST_CAN_BOARD_ID    ((uint16_t)0x000u)            // 10 bits  unique board ID
-#define CONST_CAN_RX_ID       (CONST_CAN_BOARD_ID | 0x400u) // 11 bits ID, MSb is a 1 for (Master) ->  (This)  transfers
-#define CONST_CAN_TX_ID       (CONST_CAN_BOARD_ID)         // 11 bits ID, MSb is a 0 for  (This)  -> (Master) transfers, with a higher priority
+#define CONST_CAN_RX_ID       (CONST_CAN_BOARD_ID<<1u | 0u) // 11 bits ID, LSb  is a 0 for (Master) ->  (This)  transfers
+#define CONST_CAN_TX_ID       (CONST_CAN_BOARD_ID<<1u | 1u) // 11 bits ID, LMSb is a 2 for  (This)  -> (Master) transfers, with a higher priority
 
 //BUFFER SIZE USED IN ISR
 #define CONST_CAN_BUFFER_SIZE (32)
