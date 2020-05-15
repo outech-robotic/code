348b7957b23360ff7e1a44f3a2e359c30ba5040f
Ugurcan Ekren <ugurcan.ekren@gmail.com>
2020-05-16 01:49:37 +0200

    removed dependency to configuration in odometry, send millimeters to it from position controller (#53)
    * added odometry function in utils, with tests, and fixed vector2 comparison
    
    * ran jenkins
    
    * removed dependency to configuration
    
    * removed completely dependency to configuration, sent millimeters to odometry

--- highlevel/util/odometry.py
+++ highlevel/util/odometry.py
@@ -6,14 +6,13 @@ Used to track a robot's position with sensors.
 import math
 from typing import Tuple
 
-from highlevel.robot.entity.configuration import Configuration
 from highlevel.robot.entity.type import Radian, Millimeter
 from highlevel.util.geometry.vector import Vector2
 
 
 def odometry(delta_left: Millimeter, delta_right: Millimeter,
              current_position: Vector2, current_angle: Radian,
-             configuration: Configuration) -> Tuple[Vector2, Radian]:
+             distance_between_encoder_wheels: float) -> Tuple[Vector2, Radian]:
     """
     Computes the current position and angle of the robot using the movements of its wheels.
     Uses a curvature radius to compute the new position.
@@ -26,7 +25,7 @@ def odometry(delta_left: Millimeter, delta_right: Millimeter,
 
     if delta_distance == 0:
         # Robot did not translate, rotate only.
-        wheel_dist = configuration.distance_between_wheels / 2
+        wheel_dist = distance_between_encoder_wheels / 2
         d_theta = delta_right / wheel_dist
         return current_position, current_angle + d_theta
 
@@ -38,7 +37,7 @@ def odometry(delta_left: Millimeter, delta_right: Millimeter,
 
     # Trajectory is both translation and rotation.
     # 1. Calculate the radius of curvature.
-    wheel_dist = configuration.distance_between_wheels
+    wheel_dist = distance_between_encoder_wheel
     curvature_radius = (delta_right + delta_left) / \
                        (delta_right - delta_left) * wheel_dist / 2
     # 2. Calculate the angle delta.
