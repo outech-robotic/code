bb2d84ec02bcc0c4b8b12f02c2fbb0f1b0162e04
TicTacMenthe <ugurcan.ekren@gmail.com>
2021-03-27 15:24:04 +0100

    servo_msg test

--- ros/catkin_ws/src/outech_highlevel/scripts/outech_interface_node.py
+++ ros/catkin_ws/src/outech_highlevel/scripts/outech_interface_node.py
@@ -3,26 +3,41 @@
 import rospy
 import std_msgs.msg as std_msgs
 from lib.proto.gen.python.outech_pb2 import BusMessage, ServoMsg
+from outech_highlevel.msg import Servo
+
+
+motor_publisher = None
+servo_publishers = []
+
+def callback_servo(servomsg):
+    print(servomsg)
+    servo_msg = BusMessage(servo=ServoMsg())
+    servo_msg.servo.id = servomsg.servo_id
+    servo_msg.servo.angle = servomsg.angle
+
+    buffer = servo_msg.SerializeToString()
+    servo_msg = BusMessage(servo=ServoMsg())
+    multiarray = std_msgs.UInt8MultiArray(
+    layout=std_msgs.MultiArrayLayout(dim=[std_msgs.MultiArrayDimension(label="serialized", size=0, stride=0)], data_offset=0),
+    data=[])
+
+    multiarray.data = buffer
+    multiarray.layout.dim[0].stride = multiarray.layout.dim[0].size = len(buffer)
+
+    servo_publishers[servomsg.board_id].publish(multiarray)
 
 def outech_interface_node():
-    servo_msg = BusMessage(servo=ServoMsg())
-    servo_msg.servo.id = 0
-    servo_msg.servo.angle = 45
-    direction = 4
-    multiarray = std_msgs.UInt8MultiArray(layout=std_msgs.MultiArrayLayout(dim=[std_msgs.MultiArrayDimension(label="serialized", size=32, stride=32)], data_offset=0), data=[])
-    pub = rospy.Publisher('pb_test', std_msgs.UInt8MultiArray, queue_size=10)
     rospy.init_node('outech_interface', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        servo_msg.servo.angle += direction
-        if servo_msg.servo.angle < 30 or servo_msg.servo.angle > 150:
-            direction = -direction
-        buffer = servo_msg.SerializeToString()
-        print(buffer, len(buffer))
-        multiarray.data = buffer
-        multiarray.layout.dim[0].stride = multiarray.layout.dim[0].size = len(buffer)
-        pub.publish(multiarray)
-        rate.sleep()
+    
+    motor_publisher = rospy.Publisher('bridge_motor/sent_messages', std_msgs.UInt8MultiArray, queue_size=10)
+    servo_publishers.append(rospy.Publisher('bridge_servo_0/sent_messages', std_msgs.UInt8MultiArray, queue_size=10))
+    servo_publishers.append(rospy.Publisher('bridge_servo_1/sent_messages', std_msgs.UInt8MultiArray, queue_size=10))
+    servo_publishers.append(rospy.Publisher('bridge_servo_2/sent_messages', std_msgs.UInt8MultiArray, queue_size=10))
+
+    rospy.Subscriber("servo_msg", Servo, callback_servo)
+
+    rospy.spin()
+
 
 if __name__ == '__main__':
     try:
